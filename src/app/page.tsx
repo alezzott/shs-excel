7c10775c608b54dac970115ff2ebb571b1e3bf03
Alex Faustino <faustino.alex@outlook.com>
2024-05-04 13:04:20 -0300

    style(lint): "update eslint "

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <main>
-      <section>
-        Hello NextJS !
-      </section>
-    </main>
-  );
+   return (
+      <main>
+         <section>Hello NextJS !</section>
+      </main>
+   )
 }
