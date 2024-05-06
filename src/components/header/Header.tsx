672a137b84ffb3366c5ad2f40ced0ebebe55fd99
Alex Faustino <faustino.alex@outlook.com>
2024-05-06 00:37:00 -0300

    wip: "header section"

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+import { Header } from 'antd/es/layout/layout'
+
+export const Head = () => {
+   return (
+      <section>
+         <Header style={{ color: '#f7f7f7', fontWeight: 600 }}>Header</Header>
+      </section>
+   )
+}
