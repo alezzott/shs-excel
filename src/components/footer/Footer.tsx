c8c874df77224345c3805e11caa864cabc2683f8
Alex Faustino <faustino.alex@outlook.com>
2024-05-07 20:40:58 -0300

    ref!(style): "update footer"

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,14 @@
-export const Footer = () => {
+import style from './Footer.module.scss'
+
+export const FooterComponent = () => {
    return (
-      <main>
-         <section>
-            <h2>footer</h2>
-         </section>
-      </main>
+      <section className={style['text-footer-item']}>
+         <h1>
+            Feito por{' '}
+            <a href="https://github.com/alezzott" target="_blank">
+               Alezzo
+            </a>
+         </h1>
+      </section>
    )
 }
