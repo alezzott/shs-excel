fc5f498844ba7e03d8e7030f83bfef9914512ebc
Alex Faustino <faustino.alex@outlook.com>
2024-05-07 20:37:54 -0300

    ref: "add type number"

--- src/app/api/utils/FormatCurrency.ts
+++ src/app/api/utils/FormatCurrency.ts
@@ -1,4 +1,4 @@
-export function formatToBRL(value) {
+export function formatToBRL(value: number) {
    let formattedValue = value.toString().replace('.', ',')
    formattedValue = formattedValue.replace(/\B(?=(\d{3})+(?!\d))/g, '.')
    return `R$ ${formattedValue}`
