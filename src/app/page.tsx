8a925ad5b5a95a609883d39f985dc6d64c1ef36e
Alex Faustino <faustino.alex@outlook.com>
2024-05-07 23:14:29 -0300

    chore: "alter text"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+'use client'
+
+import { TableExcelItem } from '../components/table/TableExcelItem'
+import { UploadItem } from '@/components/upload/UploadItem'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
 export default function Home() {
+   const queryClient = new QueryClient()
+
    return (
-      <main>
-         <section>Hello NextJS !</section>
-      </main>
+      <QueryClientProvider client={queryClient}>
+         <UploadItem />
+         <TableExcelItem />
+      </QueryClientProvider>
    )
 }
