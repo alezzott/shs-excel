c17dd8c79e24924f50daca01bb4a9fc7a880f712
Alex Faustino <faustino.alex@outlook.com>
2024-05-07 23:13:53 -0300

    ref: "add header section"

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,24 @@
-import { Header } from 'antd/es/layout/layout'
+import Image from 'next/image'
+import style from './Header.module.scss'
+import bannerHome from '../../assets/banner-home.svg'
 
-export const Head = () => {
+export const HeaderComponent = () => {
    return (
-      <section>
-         <Header style={{ color: '#f7f7f7', fontWeight: 600 }}>Header</Header>
+      <section className={style['text-header-section']}>
+         <section>
+            <h1>MedFileX</h1>
+            <p>
+               Facilite a sua rotina: importe do Excel com apenas alguns
+               cliques.{' '}
+            </p>
+         </section>
+         <section>
+            <Image
+               src={bannerHome}
+               alt="banner home section"
+               draggable={false}
+            />
+         </section>
       </section>
    )
 }
