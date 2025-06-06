646f18fa9e7410384b087c3ee2e2dc9229dba83a
Alex Faustino <faustino.alex@outlook.com>
2025-06-06 17:38:41 -0300

    update table types

--- src/@types/table-actions.d.ts
+++ src/@types/table-actions.d.ts
@@ -9,3 +9,13 @@ type TableActionBarProps = {
    children?: React.ReactNode
    onSelectedRows?: Row<Excel>[]
 }
+
+interface TablePaginationProps {
+   pageCount: number
+   pageIndex: number
+   canPreviousPage: boolean
+   canNextPage: boolean
+   onPreviousPage: () => void
+   onNextPage: () => void
+   onPageChange: (page: number) => void
+}
